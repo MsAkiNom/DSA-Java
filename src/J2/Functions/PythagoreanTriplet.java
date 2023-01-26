6c557e1a9ea18e3794cf65542d004f4d22d42572
MsAkiNom <77355484+MsAkiNom@users.noreply.github.com>
2023-01-26 23:41:19 +0200

    pythagorean triplet question added

---
+++ src/J2/Functions/PythagoreanTriplet.java
@@ -0,0 +1,8 @@
+package J2.Functions;
+
+public class PythagoreanTriplet {
+    public static void main(String[] args) {
+        // Write a function to check if a given triplet is a Pythagorean triplet or not.
+
+    }
+}
