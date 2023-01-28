aa90a5e9a6d932560d827963a9c225df8960f981
MsAkiNom <77355484+MsAkiNom@users.noreply.github.com>
2023-01-28 15:44:52 +0200

    pythagorean triplet solution added

--- src/J2/Functions/PythagoreanTriplet.java
+++ src/J2/Functions/PythagoreanTriplet.java
@@ -1,9 +1,26 @@
 package J2.Functions;
 
+import java.util.Scanner;
+
 public class PythagoreanTriplet {
     public static void main(String[] args) {
         // Write a function to check if a given triplet is a Pythagorean triplet or not.
-        // to-do
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        boolean isTriplet = tripletExists(a, b, c);
+        if (isTriplet) {
+            System.out.println("Triplet exists");
+        } else {
+            System.out.println("Triplet doesn't exist");
+        }
 
+    }
+
+    public static boolean tripletExists(int a, int b, int c) {
+        a = a * a; b = b * b; c = c * c;
+
+        return a == b + c || b == c + a || c == a + b;
     }
 }
