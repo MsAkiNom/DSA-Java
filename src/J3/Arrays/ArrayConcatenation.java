454999d5ffcdb4248360f82710f54037ddcd1463
MsAkiNom <77355484+MsAkiNom@users.noreply.github.com>
2023-01-29 13:15:26 +0200

    array concatenation solution added

--- src/J3/Arrays/ArrayConcatenation.java
+++ src/J3/Arrays/ArrayConcatenation.java
@@ -1,8 +1,33 @@
 package J3.Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayConcatenation {
     public static void main(String[] args) {
         // Concatenation of Array
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        System.out.println(Arrays.toString(concatenatedArray(nums)));
+
+    }
+
+    public static int[] concatenatedArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2*n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+
+        }
+
+        return ans;
     }
 }
