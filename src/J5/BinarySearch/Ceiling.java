57741792e1f6ce2323836ef71a039b7e5042461b
MsAkiNom <77355484+MsAkiNom@users.noreply.github.com>
2023-02-01 16:41:52 +0200

    ceiling number code modified

--- src/J5/BinarySearch/Ceiling.java
+++ src/J5/BinarySearch/Ceiling.java
@@ -1,21 +1,27 @@
 package J5.BinarySearch;
 
-public class CeilingNumber {
+public class Ceiling {
     public static void main(String[] args) {
         // Find a ceiling number in the array
         // Ceiling = the smallest element in array greater or equal to target
-        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
-        int target = 3;
-        int ans = ceilingNumber(arr, target);
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int target = 10;
+        int ans = ceiling(arr, target);
         System.out.println(ans);
     }
 
-    static int ceilingNumber(int[] arr, int target) {
+    static int ceiling(int[] arr, int target) {
+
+        if (target > arr[arr.length - 1]) {
+            return -1;
+        }
+
         int start = 0;
         int end = arr.length - 1;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
+
             if (target < arr[mid]) {
                 end = mid - 1;
             } else if (target > arr[mid]) {
@@ -24,6 +30,6 @@ public class CeilingNumber {
                 return mid;
             }
         }
-        return -1;
+        return start;
     }
 }
